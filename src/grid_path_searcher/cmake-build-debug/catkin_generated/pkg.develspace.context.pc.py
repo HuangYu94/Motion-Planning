b001042c2773f5dd708c868379c0161b75ea3a34
Yu Huang <565204549@qq.com>
2022-03-26 18:05:57 +0800

    init commit

---
+++ src/grid_path_searcher/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/src/grid_path_searcher/include".split(';') if "/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/src/grid_path_searcher/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "grid_path_searcher"
+PROJECT_SPACE_DIR = "/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/src/grid_path_searcher/cmake-build-debug/devel"
+PROJECT_VERSION = "0.0.1"
---
+++ src/grid_path_searcher/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/src/grid_path_searcher/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/src/grid_path_searcher/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/src/grid_path_searcher:$ROS_PACKAGE_PATH"
