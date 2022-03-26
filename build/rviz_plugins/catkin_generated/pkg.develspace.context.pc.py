a27950fa9866daac1b5d9a640ce42ca9c19cd86a
Yu Huang <565204549@qq.com>
2022-03-26 18:26:02 +0800

    init commit

--- build/rviz_plugins/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "rviz_plugins"
-PROJECT_SPACE_DIR = "/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/devel"
-PROJECT_VERSION = "0.0.0"
