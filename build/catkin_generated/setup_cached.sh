f1ac921329950e00c7ba3e60ad10fde9bc581e90
Yu Huang <565204549@qq.com>
2022-03-26 17:16:11 +0800

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/devel/lib:/opt/ros/melodic/lib:/usr/local/cuda-11.4/lib64'
+export PKG_CONFIG_PATH="/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/media/yuhuang/PSSD/gglearn/db_motion_planning/Motion-Planning-for-Mobile-Robots-main/workspace/assignments/01-env-setup/ROS/src:$ROS_PACKAGE_PATH"
